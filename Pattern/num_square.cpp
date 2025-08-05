20a4d02afd3405ca51ef6ed65d23ec0c8e2eb151
Yash Izate <yashizate.softech@gmail.com>
2025-08-05 21:38:19 +0530

    square of numbers

---
+++ Pattern/num_square.cpp
@@ -0,0 +1,24 @@
+// 1 1 1 1
+// 2 2 2 2
+// 3 3 3 3
+// 4 4 4 4
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 4;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << i + 1 << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
