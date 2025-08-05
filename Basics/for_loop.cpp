7d81d9dc506da947a58262194d509390575a9cee
Yash Izate <yashizate.softech@gmail.com>
2025-08-05 11:14:52 +0530

    for loop

---
+++ Basics/for_loop.cpp
@@ -0,0 +1,17 @@
+// your code here
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 5;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+
+    return 0;
+}
