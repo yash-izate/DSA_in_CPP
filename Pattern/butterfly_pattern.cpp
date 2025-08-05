a6d869f381c5fc234b83906860aa1da39f39f877
Yash Izate <yashizate.softech@gmail.com>
2025-08-06 00:29:34 +0530

    butterfly patterns

---
+++ Pattern/butterfly_pattern.cpp
@@ -0,0 +1,44 @@
+// your code here
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    // Upper half
+    for (int i = 1; i <= n; i++) {
+        // Left stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        // Spaces
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+        // Right stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = n; i >= 1; i--) {
+        // Left stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        // Spaces
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+        // Right stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
