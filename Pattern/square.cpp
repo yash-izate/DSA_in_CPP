f276bd633052d8f9a0f5831b66f9920eaeff641e
Yash Izate <yashizate.softech@gmail.com>
2025-08-05 11:42:46 +0530

    square pattern

---
+++ Pattern/square.cpp
@@ -0,0 +1,22 @@
+// your code here
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 5;
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            cout << " * ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
