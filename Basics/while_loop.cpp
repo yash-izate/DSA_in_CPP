a24aea3d3a7d47e12767f10596259cfd0ac5b709
Yash Izate <yashizate.softech@gmail.com>
2025-08-05 11:38:26 +0530

    while loop added

---
+++ Basics/while_loop.cpp
@@ -0,0 +1,18 @@
+// your code here
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i = 1;
+
+    while (i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
