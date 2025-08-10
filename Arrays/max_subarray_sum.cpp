434361be0b941a43c4b8844d6cc648380b81b132
Yash Izate <yashizate.softech@gmail.com>
2025-08-11 01:57:42 +0530

    finding max subarray sum

---
+++ Arrays/max_subarray_sum.cpp
@@ -0,0 +1,41 @@
+// Brute Force Approach
+
+#include <iostream>
+using namespace std;
+
+int maxSubarraySum(int *arr, int n)
+{
+    int maxSum = INT_MIN;
+
+    for (int start = 0; start < n; start++)
+    {
+        for (int end = start; end < n; end++)
+        {
+            int currSum = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                currSum += arr[i];
+            }
+
+            cout << currSum << ", ";
+            maxSum = max(maxSum, currSum);
+        }
+
+        cout << endl;
+    }
+
+    return maxSum;
+}
+
+int main()
+{
+
+    int arr[] = {2, -3, 6, -5, 4, 2};
+    int n = size(arr);
+
+    int result = maxSubarraySum(arr, n);
+    cout << "Max Subarray Sum: " << result << endl;
+
+    return 0;
+}
