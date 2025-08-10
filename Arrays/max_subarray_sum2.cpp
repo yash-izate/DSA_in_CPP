b982ad6c01bb9fe75d33e8f645b8afe44233e97d
Yash Izate <yashizate.softech@gmail.com>
2025-08-11 02:20:43 +0530

    added optimized max subarray sum

--- Arrays/max_subarray_sum2.cpp
+++ Arrays/max_subarray_sum2.cpp
@@ -1,10 +1,38 @@
-// your code here
+// Optimized Approach - Time Complexity O(n^2)
 
 #include <iostream>
 using namespace std;
 
-int main() {
-    
- 
+int maxSubarraySum(int *arr, int n)
+{
+    int maxSum = INT_MIN;
+
+    for (int start = 0; start < n; start++)
+    {
+        int currSum = 0;
+
+            for (int end = start; end < n; end++)
+        {
+            currSum += arr[end];
+            maxSum = max(maxSum, currSum);
+
+            cout << currSum << ", ";
+        }
+
+        cout << endl;
+    }
+
+    return maxSum;
+}
+
+int main()
+{
+
+    int arr[] = {2, -3, 6, -5, 4, 2};
+    int n = size(arr);
+
+    int result = maxSubarraySum(arr, n);
+    cout << "Max Subarray Sum: " << result << endl;
+
     return 0;
 }
