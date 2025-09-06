68027e913be2d7ea10c5f6226277f39f2f199a5c
Yash Izate <yashizate.softech@gmail.com>
2025-09-06 16:54:27 +0530

    2d array added

---
+++ Arrays_2D/marks.cpp
@@ -0,0 +1,16 @@
+// your code here
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+                    //  a0   a1  a2 
+    int marks[3][3] = {{100, 90, 80},    // b0
+                       {98, 88, 78},     // b1
+                       {45, 100, 58}};   // b2
+
+    cout << marks[2][0] << endl;
+
+    return 0;
+}
