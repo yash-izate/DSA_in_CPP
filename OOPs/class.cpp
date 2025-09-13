c6bf4e756bf3c4a182976891377b8bdb9c2d0540
Yash Izate <yashizate.softech@gmail.com>
2025-09-13 18:23:49 +0530

    upate

---
+++ OOPs/class.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+class Hero {
+    // properties of class
+    int health;
+};
+
+int main() {
+    // object of class
+    Hero h1;
+    cout << "size of h1: " << sizeof(h1) << endl;
+    return 0;
+}
