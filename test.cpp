0bc91ee1f32888c3c309d4e081e7803c5d78f4d1
Yash Izate <yashizate.softech@gmail.com>
2025-09-27 14:30:10 +0530

    fixed input output txt issues

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age;
+    string name;
+
+    cout << "Enter your name: \n"<< endl;
+    cin >> name;
+    cout << "Enter age: \n" << endl;
+    cin >> age;
+    
+    cout <<"Your name is " << name << endl;
+    cout << "Your age is " << age << endl;
+
+    return 0;
+} 
